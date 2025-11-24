7b6e617a9d91011bf405c5376afea100c9a1a203
Alexei-Ch <billy.solitaire@gmail.com>
2025-11-24 14:43:38 +0300

    Корректировка гайда, скрипта

--- daily-Alexei.sh
+++ daily-Alexei.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git remote set-url origin git@github-Alexei:Metrologia-code/PyProjects.git
 git config user.name "Alexei-Ch"
 git config user.email "billy.solitaire@gmail.com"
 eval $(ssh-agent -s)
