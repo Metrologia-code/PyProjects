cf8c439163bac1ed251b84761d250c004b1d5c36
Luparev <luparev@gmail.com>
2026-05-21 13:20:28 +0300

    Исправил пересчет сопротивления в температуру

--- User_libs/pyTools.py
+++ User_libs/pyTools.py
@@ -7,9 +7,31 @@ class Transformation():
         
         self.Transforms = {'RES2T': self.Trasform_RES_to_Temp, }
 
-    def Trasform_RES_to_Temp(self, y_data, Rc=0, **kwargs):
+    def Trasform_RES_to_Temp(self, y_data, R0=100, Rc=0, **kwargs):
         #преобразование сопротивления в температуру
-        return [((y-Rc)/100-1)/0.00385 for y in y_data]
+        return [self._RTD_Pt_res_to_temp(y, R0=R0, Rc=Rc) for y in y_data]
+
+    @staticmethod
+    def _RTD_Pt_res_to_temp(R, /, R0=100, Rc=0):
+        a = 3.9083 * 10 ** -3
+        b = -5.775 * 10 ** -7
+        c = -4.183 * 10 ** -12
+
+        if R >= R0:
+            # для положительных температур решаем квадратное уравнение R = Rc + R0 * (1 + a * t + b * t^2)
+            d = a ** 2 - 4 * b * (R0 - R + Rc) / R0
+            t = (d ** 0.5 - a) / (2 * b)
+            return t
+        else:
+            # для отрицательных температур решение ищем методом Ньютона-Рафсона (хватает 2-3 итераций)
+            # уравнение R = Rc + R0 * (1 + a * t + b * t^2 + c * (t - 100) * t^3)
+            t = (R - Rc - R0) / (a * R0)
+            while True:
+                f_t = (c * t ** 4 - 100 * c * t ** 3 + b * t ** 2 + a * t + 1) * R0 + Rc - R
+                df_dt = (4 * c * t ** 3 - 300 * c * t ** 2 + 2 * b * t + a) * R0
+                t -= f_t / df_dt
+                if abs(f_t / df_dt) < 0.0001:
+                    return t
 
 def ParseLoggerArguments(LaunchArguments=None, ):
     ''' принимает список из аргументов в формате строк
